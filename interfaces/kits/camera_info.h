827347c0669cb81622aae5aed25290e9bc61ef8a
杨帅 <yangshuai67@huawei.com>
2021-08-16 06:47:01 +0000

    update interfaces/kits/camera_info.h. Signed-off-by: mamingshuai <mamingshuai1@huawei.com>

--- interfaces/kits/camera_info.h
+++ interfaces/kits/camera_info.h
@@ -1,12 +1,4 @@
 /*
- * @Author: your name
- * @Date: 2021-08-16 11:33:08
- * @LastEditTime: 2021-08-16 12:08:30
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \multimedia_camera_lite\interfaces\kits\camera_info.h
- */
-/*
  * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
