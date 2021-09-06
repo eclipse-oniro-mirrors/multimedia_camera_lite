a16c5b0c1a5011fa209a90eda791280ae215db4c
杨帅 <yangshuai67@huawei.com>
2021-09-06 12:35:47 +0000

    Signed-off-by: yangshuai<yangshuai67@huawei.com>

--- services/src/camera_device.cpp
+++ services/src/camera_device.cpp
@@ -46,7 +46,7 @@ const int32_t KEY_TIME_US = 2;
 const int32_t IMAGE_WIDTH = 3;       // "DATA_PIX_FORMAT"
 const int32_t IMAGE_HEIGHT = 4;       // "DATA_PIX_FORMAT"
 const int32_t IMAGE_SIZE = 5;       // "DATA_PIX_FORMAT"
-const int32_t DELAY_TIME_ONE_FRAME = 30000；
+const int32_t DELAY_TIME_ONE_FRAME = 30000;
 
 namespace OHOS {
 namespace Media {
